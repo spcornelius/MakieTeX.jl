6e3df249d7e4c943b7cc3252eb459bb4ec33a2d7
Sean P. Cornelius <spcornelius@gmail.com>
2023-02-24 13:51:22 -0500

    fix bug

--- src/recipe.jl
+++ src/recipe.jl
@@ -63,7 +63,7 @@ function Makie.plot!(plot::T) where T <: TeXImg
     # We always want to draw this at a 1:1 ratio, so increasing scale or
     # changing dpi should rerender
     plottable_images = lift(plot[1], plot.render_density, plot.scale) do cachedtex, render_density, scale
-        to_array(firstpage2img((cachedtex); render_density = render_density * scale))
+        to_array(firstpage2img.(cachedtex; render_density = render_density * scale))
     end
 
     scatter_images    = Observable(plottable_images[])
